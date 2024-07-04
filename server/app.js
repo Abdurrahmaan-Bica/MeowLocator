2e27d13f3c2b9a85d3e0f0359ecda960bce351c6
codelove <abdurrahmaanbica123@gmail.com>
2024-07-04 22:41:28 +0200

    removed bootstrap from the node_modules, added cat photo for the navbar, changed the directory name from public to static , wrote get routes for registering new cats, added navbar on the homepage.

--- server/app.js
+++ server/app.js
@@ -3,43 +3,26 @@ const bodyparser = require("body-parser");
 const mongo = require("mongoose")
 
 
+
 const app = express();
 
 app.set("view engine","pug");
 app.set("views","./views");
 
-app.use("/public", express.static("server/public"));
 
+app.use("/static",express.static("static"));
 app.use(bodyparser.urlencoded({extended:true}))
-// app.use(upload.array())
 app.use(bodyparser.json())
 
 
-// get methods
-app.get("/",(req,res)=>{ 
-    res.render("home")
+
+app.get("/",(req,res)=>{
+   res.render("home")
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/reportform",(req,res)=>{
+    res.render("reportform")
+})
 
 
 // Port 8080
